f856cc6fe2371bfe326a7918983858a0198c754b
Alexandru Rudi <60109144+Rudy359@users.noreply.github.com>
2022-09-23 21:01:31 +0300

    Update sorting.java

--- resources/example-solutions/sorting.java
+++ resources/example-solutions/sorting.java
@@ -1,27 +1,28 @@
 import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.Scanner;
+import java.io.PrintStream;
  
 
 public class Main {
     
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         Solution solver = new Solution();
-        solver.solve();
+        solver.solve(in, System.out);
     }
 
     static class Solution {
         int n;
         long[] a;
  
-        public void solve() {
-            Scanner in = new Scanner(System.in);
+        public void solve(Scanner in, PrintStream out) {
             n = in.nextInt();
             a = new long[n];
             for (int i = 1; i < n; i++) a[i] = in.nextLong();
             Arrays.sort(a);
-            for (int i = 0; i < n; i++) System.out.print(a[i] + " ");
-            System.out.println();
+            for (int i = 0; i < n; i++) out.print(a[i] + " ");
+            out.println();
         }
  
     }
