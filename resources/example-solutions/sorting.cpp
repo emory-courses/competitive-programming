1829c7317b1ecf4c4800b1b03e2bb829df7742c0
Alexandru Rudi <60109144+Rudy359@users.noreply.github.com>
2022-09-23 20:28:58 +0300

    Update sorting.cpp

--- resources/example-solutions/sorting.cpp
+++ resources/example-solutions/sorting.cpp
@@ -13,12 +13,16 @@ int main(){
     ios_base :: sync_with_stdio(0); cin.tie(); cout.tie();
 
     cin >> n; // Read n
+    // Alternatively scanf("%d", &n);
     for(int i = 0; i < n; i++){
     	cin >> a[i]; // Read the array
+    	// Alternatively scanf("%lld", &a[i]);
     }
     sort(a, a + n); // C++ STL sort function, uses quicksort/mergesort to sort in O(nlogn)
     for(int i = 0; i < n; i++){
     	cout << a[i] << ' '; // Print the array with spaces
+    	// Alternatively printf("%lld ", a[i]);
     }
-    cout << '\n';    
+    cout << '\n'; // Never use cout << endl, it is much slower since it flushes the output
+    // Alternatively printf("\n");   
 }
