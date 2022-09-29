b05e61c419b165c8c1b7a8081f9853379aa7ef43
Alexandru Rudi <60109144+Rudy359@users.noreply.github.com>
2022-09-29 21:49:02 +0300

    Update and rename main.cpp to binary-search.cpp

--- resources/example-solutions/meeting-2/binary-search.cpp
+++ resources/example-solutions/meeting-2/binary-search.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, a[100005], k;
+int n, a[100005], k, l, r, t;
 
 int main(){
     ios_base :: sync_with_stdio(0); cin.tie(); cout.tie();
@@ -13,7 +13,7 @@ int main(){
     cin >> k;
 
     // First element >= k
-    int l = 0, r = n - 1;
+    l = 0, r = n - 1;
     while(l != r){
     	int mid = (l + r) / 2;
     	if(a[mid] < k) l = mid + 1;
@@ -22,7 +22,8 @@ int main(){
     if(a[l] < k) cout << "No answer\n";
     else cout << a[l] << '\n';
 
-    // First element <= k
+
+    // Last element <= k
     l = 0, r = n - 1;
     while(l != r){
     	int mid = (l + r + 1) / 2;
@@ -32,5 +33,22 @@ int main(){
     if(a[l] > k) cout << "No answer\n";
     else cout << a[l] << '\n';
 
+
+    // First element >= k
+    t = n - 1;
+    for(int i = 20; i >= 0; i--){
+    	if(t - (1 << i) >= 0 && a[t - (1 << i)] >= k) t -= (1 << i);
+    }
+    if(a[t] < k) cout << "No answer\n";
+    else cout << a[t] << '\n';
+
+
+    // Last element <= k
+    t = 0;
+    for(int i = 20; i >= 0; i--){
+    	if(t + (1 << i) < n && a[t + (1 << i)] <= k) t += (1 << i);
+    }
+    if(a[t] > k) cout << "No answer\n";
+    else cout << a[t] << '\n';
 
 }
